8146fb9b0daa2c1c42f6f8eb878375115c64ad44
Robert J. Kelly <rjkelly@Roberts-MacBook-Pro.local>
2016-11-03 18:13:03 -0400

    final homework

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static('app/public'));
 
 // ROUTER //
 
